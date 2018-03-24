c41dc65e3eca7d17750c714a8747460605ea8fbb
azizkastalli <azizkastalli@esprit.tn>
2018-03-24 16:38:28 +0100

    Merge origin/master into aziz
    Conflicts:
    	nbproject/project.properties

--- src/gui/controller/MenuController.java
+++ src/gui/controller/MenuController.java
@@ -95,12 +95,11 @@ public class MenuController {
         Scene home_page_scene = new Scene(home_page_parent);
         Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
                       
-                app_stage.hide(); //optional
                 app_stage.setScene(home_page_scene);
                 app_stage.show();  
        
         }
        
           }
-    
+   
 }
