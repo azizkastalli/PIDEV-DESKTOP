95f9fed8bdb6930ec7b14011bc4502ff340e517a
HP <HP@172.16.243.237>
2018-03-26 17:41:21 +0100

    crud produit

--- src/gui/controller/AjouterStoreController.java
+++ src/gui/controller/AjouterStoreController.java
@@ -6,13 +6,23 @@
 package gui.controller;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import entites.Produit;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import services.CrudProduit;
 
 /**
  * FXML Controller class
@@ -43,6 +53,32 @@ public class AjouterStoreController implements Initializable {
     private FontAwesomeIconView gererenchersicon;
     @FXML
     private Label espace;
+    @FXML
+    private TextField nomproduit;
+    @FXML
+    private TextField caracteristique;
+    @FXML
+    private TextField description;
+    @FXML
+    private TextField etat;
+    @FXML
+    private TextField categorie;
+    @FXML
+    private TextField proprietaire;
+    @FXML
+    private TextField vote;
+    @FXML
+    private TextField quantite;
+    @FXML
+    private TextField prixnouv;
+    @FXML
+    private TextField prixanc;
+    @FXML
+    private TextField poid;
+    @FXML
+    private TextField image;
+    @FXML
+    private Button ajoutproduit;
 
     /**
      * Initializes the controller class.
@@ -63,5 +99,49 @@ public class AjouterStoreController implements Initializable {
            MenuController menu = new MenuController();
         menu.GestionMenuEspace(event);
     }
+
+    @FXML
+    private void ajoutproduit(ActionEvent event) throws IOException {
+        CrudProduit CP = new CrudProduit();
+        Produit PR = new Produit();
+        PR.setLabel(nomproduit.getText());
+        PR.setCaracteristiques(caracteristique.getText());
+        PR.setDescription(description.getText());
+        PR.setEtat(etat.getText());
+        int idcat= Integer.parseInt(categorie.getText());
+        PR.setId_categorie(idcat);
+        int idprop = Integer.parseInt(proprietaire.getText()) ;
+        PR.setId_propietaire(idprop);
+        PR.setNom_image(image.getText());
+        double poi=Double.parseDouble(poid.getText());
+        PR.setPoid(poi);
+        double prian=Double.parseDouble(prixanc.getText());
+        double prinouv=Double.parseDouble(prixnouv.getText());
+        PR.setPrix_ancien(prian);
+        PR.setPrix_nouv(prinouv);
+        int quant= Integer.parseInt(quantite.getText());
+        PR.setQuantite(quant);
+        double vot=Double.parseDouble(vote.getText());
+        PR.setVote(vot);
+        
+        
+        
+        
+         try {
+            CP.Create(PR);
+             System.out.println("produit ajouté");
+        } catch (SQLException ex) {
+        }
+        
+          
+    }
+    
+    
+
+    
+    
+        
+        
+        
     
 }
